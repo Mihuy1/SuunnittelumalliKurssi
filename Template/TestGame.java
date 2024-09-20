b6aa046ab6262a9f8d73e6582d8c3118d7f3043a
Mihuy1 <patrik.hyytiainen@protonmail.com>
2024-09-20 14:04:58 +0300

    feat: Add Template Design Pattern

---
+++ Template/TestGame.java
@@ -0,0 +1,11 @@
+package Template;
+
+public class TestGame {
+
+    public static void main(String[] args) {
+        // Test Rock, Paper, Scissors
+        Game rockPaperScissors = new RockPaperScissors();
+        rockPaperScissors.play(2);
+    }
+
+}
