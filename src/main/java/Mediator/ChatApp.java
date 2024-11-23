f0a5950beae6051315b1c816e79bb1deed147fec
Mihuy1 <patrik.hyytiainen@gmail.com>
2024-11-23 19:56:08 +0200

    fix: Fix flyweight dp

---
+++ src/main/java/Mediator/ChatApp.java
@@ -0,0 +1,15 @@
+package Mediator;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ChatApp extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        
+    }
+}
